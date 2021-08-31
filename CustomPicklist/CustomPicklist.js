dacbdb85a0ddb1aca1092a1b7c2143e4aeffe8b6
LFGaius <53369728+LFGaius@users.noreply.github.com>
2021-08-31 11:33:00 -0700

    Update CustomPicklist.js

--- CustomPicklist/CustomPicklist.js
+++ CustomPicklist/CustomPicklist.js
@@ -1,15 +1,23 @@
 import { LightningElement,api } from 'lwc';
 
+const DEFAULT_PLACEHOLDER='Select option...'
 export default class BnPosConsoleCustomPicklistNew extends LightningElement {
     @api options=[
         {value:'1',label:'1'},
         {value:'2',label:'2'}
     ]
+    @api variant='';
+    @api placeholder=DEFAULT_PLACEHOLDER;
     optionsVisible=true;
-    valueSelected='1'
+    valueSelected;
+    
 
     get labelSelected(){
         return this.options.find(e=>e.value==this.valueSelected)?.label
+    }
+
+    get boxValueClass(){
+        return `slds-input_faux slds-combobox__input slds-align_absolute-center slds-text-align_left ${this.optionsVisible?'slds-has-focus':''} ${this.variant}`
     }
 
     onClickInput(event){
@@ -18,17 +26,26 @@ export default class BnPosConsoleCustomPicklistNew extends LightningElement {
     }
 
     onSelectOption(event){debugger
-        this.valueSelected=event.currentTarget.dataset.id
         this.optionsVisible=false
         event.stopPropagation()//We stop propagation for the document to not listen to the click event
+        if(this.valueSelected!=event.currentTarget.dataset.id){
+            this.valueSelected=event.currentTarget.dataset.id
+            this.dispatchEvent(new CustomEvent('change',{
+                detail:this.options.find(e=>e.value==this.valueSelected)
+            }))
+        }
     }
 
     /**
      * the document onclick callback is used here to listen to any clicks outside the combobox
      */
     renderedCallback(){
+        if(this.template.querySelector('.slds-dropdown'))
+         this.template.querySelector('.slds-dropdown').style.width=`${this.template.querySelector('.slds-combobox').offsetWidth}px`
         document.addEventListener('click', ()=>{
             if(this.optionsVisible) this.optionsVisible=false
         })
     }
+
+    
 }
